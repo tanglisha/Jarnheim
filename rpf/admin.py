03cf7f457522966ea7d9d15783a5076a334e5afe
Liz Dahlstrom <tanglisha@smartass.local>
2011-12-30 19:57:19 -0800

    Created a generic sqlite install in settings.py.
    Added model pulls to /rpf/admin.py

--- rpf/admin.py
+++ rpf/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
 # import all the models from the RPF
-from jarnheim.rpf.models import *
+#from jarnheim.rpf.models import *
+from Jarnheim.rpf.race import *
+from Jarnheim.rpf.ban import *
+from Jarnheim.rpf.character import *
+from Jarnheim.rpf.guild import *
 
 admin.site.register(race)
 admin.site.register(ban)
