5f46fe611a027427f2cccfbf31a51754e3c215a7
Liz Dahlstrom <tanglisha@smartass.local>
2011-12-31 15:27:27 -0800

    Added a couple of user stories

--- rpf/admin.py
+++ rpf/admin.py
@@ -1,11 +1,7 @@
 from django.contrib import admin
 
 # import all the models from the RPF
-#from jarnheim.rpf.models import *
-from Jarnheim.rpf.race import *
-from Jarnheim.rpf.ban import *
-from Jarnheim.rpf.character import *
-from Jarnheim.rpf.guild import *
+from Jarnheim.rpf.models import *
 
 admin.site.register(race)
 admin.site.register(ban)
---
+++ rpf/models.py
@@ -0,0 +1,5 @@
+#from jarnheim.rpf.models import *
+from Jarnheim.rpf.race import *
+from Jarnheim.rpf.ban import *
+from Jarnheim.rpf.character import *
+from Jarnheim.rpf.guild import *
