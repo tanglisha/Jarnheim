1174f97d484d8b5acc039bdfe83c680e9393d036
Liz Dahlstrom <tanglisha@smartass.local>
2011-12-31 15:28:33 -0800

    Added some extra race info

--- rpf/race.py
+++ rpf/race.py
@@ -2,8 +2,10 @@ from django.db import models
 
 class race( models.Model ):
     id = models.AutoField( primary_key = True )
-    create_dtm = models.DateTimeField()
-    name = models.CharField( max_length = 30 )
+    create_dtm = models.DateTimeField(verbose_name="Date created",
+                                      db_index=False)
+    name = models.CharField( max_length = 30,
+                             db_index=False)
 
     def __unicode__( self ):
         return self.name
