e9e49a729296a3bcf1718b2764cdf11de021e0cf
Liz Dahlstrom <tanglisha@smartass.local>
2011-12-31 15:27:58 -0800

    Added some extra info to guild

--- rpf/guild.py
+++ rpf/guild.py
@@ -3,7 +3,8 @@ from django.db import models
 class guild( models.Model ):
     id = models.AutoField( primary_key = True )
     guild_leader_character = models.ForeignKey( 'character' )
-    create_dtm = models.DateTimeField()
+    create_dtm = models.DateTimeField(verbose_name="Date created",
+                                      db_index=False)
     name = models.CharField( max_length = 30 )
     
 
