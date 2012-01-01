1b633b574c95ea61a2300a4ea2d04c828772b913
Liz Dahlstrom <tanglisha@smartass.local>
2011-12-31 22:03:59 -0800

    Added another sanity check test

--- requirements/testFormulas.py
+++ requirements/testFormulas.py
@@ -13,6 +13,10 @@ class TestCompute1rm(unittest.TestCase):
         self.set.weight = 100
 
     def test0reps(self):
+        """Sanity check:
+        0 reps should return whatever the old rep max was.
+        The set is considered incomplete.
+        """
         self.set.reps = 0
         new1rm = compute1rm(self.set)
         self.assertEqual(new1rm, 0, "1rm should be 0 - set was not completed")
@@ -20,6 +24,11 @@ class TestCompute1rm(unittest.TestCase):
         self.assertEqual(newer1rm, 100, "1rm should remain 100")
 
     def test1rep(self):
+        """Sanity check:
+        1 rep will always change the max to that weight if it's
+        more than the old 1rm.  If it's not, the 1rm remains
+        unchanged.
+        """
         self.set.reps = 1
         new1rm = compute1rm(self.set, 100)
         self.assertEqual(new1rm, 100, "1rm should remain 100")
@@ -27,10 +36,22 @@ class TestCompute1rm(unittest.TestCase):
         self.assertEqual(newer1rm, 100, "1rm should now be 100")
         
     def testSeveralReps(self):
+        """Formula check"""
         maxComputedMethod = BRZYCKI
         self.set.reps = 2
         twoRepsMax = compute1rm(self.set)
         self.assertEqual(twoRepsMax, 95, "1rm should now be 95")
+
+    def testWayTooManyReps(self):
+        """Sanity check:
+        Lots of reps means the calculators become fairly useless.
+        We can go ahead and change the max to whatever was done,
+        but there's no real value in trying to figure out what the
+        max would be.
+        """
+        self.set.reps = 200
+        twohundredRepsMax = compute1rm(self.set)
+        self.assertEqual(twohundredRepsMax, 100, "1rm should now be 100")
 
 if "__main__" == __name__:
     unittest.main()
